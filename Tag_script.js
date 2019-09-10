aa5dd501e752e066da96586e4fe5ec6675e6d16f
FAFANADOR <rodriguez.fernando@uqam.ca>
2019-09-10 11:16:19 -0400

    Sauvegarder le fichier log

--- Tag_script.js
+++ Tag_script.js
@@ -2,3 +2,8 @@ if (!document.hasAspect("cm:taggable"))
 document.addAspect("cm:taggable");
 document.addTag("2012-2013");
 document.addTag("Reunion ordinaire")
+
+//guardamos el archivo txt con el registro
+logFile.content = salida;
+logFile.save();
+logFile.content;
