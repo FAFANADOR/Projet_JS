b51be43c25ba7eb0ac7d0bd1a7ba06b512396900
FAFANADOR <rodriguez.fernando@uqam.ca>
2019-09-10 11:19:13 -0400

    Changer la periode à 2018-2019

--- Tag_script.js
+++ Tag_script.js
@@ -1,6 +1,6 @@
 if (!document.hasAspect("cm:taggable"))
 document.addAspect("cm:taggable");
-document.addTag("2012-2013");
+document.addTag("2018-2019");
 document.addTag("Reunion ordinaire")
 
 //guardamos el archivo txt con el registro
