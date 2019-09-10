5e4a4cd478936e20dac68f70df77da28a988dedd
FAFANADOR <rodriguez.fernando@uqam.ca>
2019-09-10 11:17:33 -0400

    Crrection node raisin

--- Chercher_node.js
+++ Chercher_node.js
@@ -1,4 +1,5 @@
-var rootSpaceName_1 = companyhome.childByNamePath( node_raiz_1 );
+var rootSpaceName_1 = companyhome.childByNamePath( node_raiz_0 );
 var childList_1 = rootSpaceName_1.children;
 test[1] = childList_1[0].properties.name;
 salida += "node 1  " + test[1] + "\n\r";
+
